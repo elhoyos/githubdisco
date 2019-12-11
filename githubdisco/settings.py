dd0b64626abfcc7a1435d7ec1427518be1387d34
Juan Hoyos <juan.hoyosr@gmail.com>
2019-12-11 11:29:51 -0500

    Wait a little less

--- githubdisco/settings.py
+++ githubdisco/settings.py
@@ -39,7 +39,7 @@ CONCURRENT_REQUESTS = 6 # Set to your number of tokens
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 1 # 5000 req/hour => 0.72 sec/req
+DOWNLOAD_DELAY = 0.9 # 5000 req/hour => 0.72 sec/req
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
